7466faca24caa6cc70c5c283885941d38dad7f45
Harrf Akbar <harrf_akbar@yahoo.com>
2021-06-06 11:55:43 -0700

    minor changes

--- app.js
+++ app.js
@@ -2,13 +2,24 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
+const address = process.argv[2]
 
-forecast(37.8045, -122.2714, (error, data) => {
-    console.log(error)
-    console.log(data)
-})
+if (!address) {
+    console.log('Please provide an address')
+} else {
+    geocode(address, (error, data) => {
+        if (error) {
+            return console.log(error)
+        }
+        
+        forecast(data.lat, data.long, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
 
-geocode('Oakland', (error, data) => {
-    console.log(error)
-    console.log(data)
-})
